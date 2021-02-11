acb7325bfc983a06e35347d2914c35d66d1199a5
drepina <darya.drepina@kaspersky.com>
2021-02-11 22:20:46 +0300

    upd test_get

--- tests/test_post_posts.py
+++ tests/test_post_posts.py
@@ -1,4 +1,3 @@
-import allure
 from framework.check import *
 from framework.helper import *
 from framework.jsonplaceholder_client import Client
@@ -8,7 +7,7 @@ from framework.jsonplaceholder_client import Client
 @allure.suite('POST')
 class TestSendPosts:
     @allure.title('Positive. Add new post')
-    def test_positive_add_new_post(self, generate_post):
-        data_json = generate_post
+    def test_positive_add_new_post(self):
+        data_json = generate_post()
         response = Client().post_new_posts(data_json)
         check_add_new_post(data_json, response)
